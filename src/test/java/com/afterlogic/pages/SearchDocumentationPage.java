ac4811ee9a64c490eee67e1c8d536484cea14ac0
Elena Sokolova <>
2023-09-04 16:13:57 +0200

    moved variables to TestData

--- src/test/java/com/afterlogic/pages/SearchDocumentationPage.java
+++ src/test/java/com/afterlogic/pages/SearchDocumentationPage.java
@@ -3,16 +3,13 @@ package com.afterlogic.pages;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
+import static com.afterlogic.tests.TestData.*;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchDocumentationPage {
-
-    String documentationList = "Online documentation";
-    String searchKey = "Two-factor";
-    String searchResultText = "Two-factor authentication";
 
     SelenideElement
             supportTab = $(".item106"),
--- src/test/java/com/afterlogic/tests/TestData.java
+++ src/test/java/com/afterlogic/tests/TestData.java
@@ -2,9 +2,29 @@ package com.afterlogic.tests;
 
 import com.github.javafaker.Faker;
 
+import java.util.List;
+
 import static com.afterlogic.utils.RandomUtils.getRandomMessage;
 
 public class TestData {
+
+    public static String successOpenPageText = "Afterlogic Corp. is an award-winning technological company",
+            productsListName = "All Products",
+            contactFormName = "Contact",
+            successSendMessageText = "Thank you for your inquiry.\n" +
+                    "Your message will be processed as soon as possible.",
+            newsSectionName = "News",
+            newsTitle = "Release of Aurora Corporate and WebMail Pro 9.7.1",
+            privacyPolicyTitleName = "Privacy policy",
+            documentationList = "Online documentation",
+            searchKey = "Two-factor",
+            searchResultText = "Two-factor authentication";
+
+    public static List<String> tabsTitles = List.of("Products", "Purchase", "Support", "Our Clients", "Services",
+            "About"),
+            productNames = List.of("Afterlogic Aurora Corporate", "Afterlogic WebMail Pro PHP",
+                    "Afterlogic WebMail Pro ASP.NET", "Afterlogic ActiveServer", "Afterlogic MailSuite Pro for Linux",
+                    "MailBee.NET Objects", "MailBee Objects");
 
     Faker faker = new Faker();
 
